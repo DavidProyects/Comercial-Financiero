0b93f656dea6289bb1a776273ff5ae82ffba21df
DavidProyects <95661957+DavidProyects@users.noreply.github.com>
2022-01-24 21:46:05 -0500

    MAIN

--- User/Scrip/User.js
+++ User/Scrip/User.js
@@ -1,5 +1,5 @@
 function description1(){
-    swal("La memoria RAM es la memoria principal de un dispositivo, esa donde se almacenan de forma temporal los datos de los programas que estás utilizando en este momento. Sus siglas significan Random Access Memory, lo que traducido al español sería Memoria de Acceso Aleatorio, y es un tipo de memoria que te puedes encontrar en cualquier dispositivo, desde ordenadores de sobremesa hasta teléfonos móviles. La memoria RAM tiene dos características que la diferencian del resto de tipos de almacenamiento. Por una parte tiene una enorme velocidad, y por otra los datos sólo se almacenan de forma temporal. Esto quiere decir que cada vez que reinicies o apagues tu ordenador, lo normal es que los datos que haya almacenados en la RAM se pierdan.")
+    swal(Image)
 }
 function description2(){
     swal("El ratón hace parte de los periféricos de tu equipo y es la parte del computador que te permite interactuar con los objetos que aparecen en la pantalla, por medio de un cursor o puntero que verás en el monitor.")
